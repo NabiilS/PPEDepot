4a9b28996d8000739c9aabab46b8ef83b124c1d5
Nabil S <nabil.so@live.fr>
2020-09-23 16:30:02 +0200

    Ajout de bdd final

--- BDDRallyeAlterTable.sql
+++ BDDRallyeAlterTable.sql
@@ -27,23 +27,20 @@ add constraint fk_question_idLivre foreign key (idLivre) references livre(idLivr
 Alter table proposition
 add constraint pk_Proposition primary key (idProposition);
 
-Alter table question
-add constraint fk_proposition_idQuestion foreign key (idQuestion) references question (idQuestion);
-
 /*--- Alter table rallye --- */
 Alter table rallye
 add constraint pk_rallye primary key (idRallye); 
 
 /*--- Alter table comporter --- */
 alter table comporter 
-add constraint pk_comporter_idLivre foreign key (idLivre) references Livre (idLivre),
-add constraint pk_comporter_idRallye foreign key (idRallye) references Rallye (idRallye);
+add constraint fk_comporter_idLivre foreign key (idLivre) references Livre (idLivre),
+add constraint fk_comporter_idRallye foreign key (idRallye) references Rallye (idRallye);
 
 /*--- Alter table classe --- */
 alter table classe 
 add constraint pk_classe primary key (idClasse),
-add constraint fk_classe_idEnseignant foreign key (idEnseignant) references enseignant(idEnseignant),
-add constraint fk_classe_idNiveau foreign key (idNiveau) references niveau (idEnseignant);
+add constraint fk_classe_idNiveau foreign key (idNiveau) references niveau (idNiveau),
+add constraint fk_classe_idEnseignant foreign key (idEnseignant) references enseignant(idEnseignant);
 
 /*--- Alter table niveau --- */
 alter table niveau
@@ -73,6 +70,134 @@ add constraint fk_reponse_IdProposition foreign key (idProposition) references p
 alter table enseignant
 add constraint pk_enseignant primary key (idEnseignant);
 
+
+
+/* création de la database */
+drop DATABASE if exists RallyeLecture;
+CREATE DATABASE RallyeLecture;
+
+/* on se positionne sur la base Rallye */
+use RallyeLecture;
+
+
+/* suppression puis création de la table livre */
+drop table if exists livre;
+
+create table livre(
+idLivre int not null auto_increment,
+titre varchar (45),
+couverture varchar (100),
+idAuteur int,
+idEditeur int,
+index (idLivre))engine=innodb;
+
+/* suppression puis création de la table éditeur */
+drop table if exists editeur;
+
+create table editeur(
+idEditeur int auto_increment not null,
+nom varchar (60),
+index (idEditeur))engine=innodb;
+
+/* suppression puis création de la table auteur */
+drop table if exists auteur;
+
+create table auteur(
+idAuteur int auto_increment not null,
+nom varchar (60),
+index (idAuteur))engine=innodb;
+
+/* suppression puis création de la table question */
+drop table if exists question;
+
+create table question(
+idQuestion int not null auto_increment,
+question varchar (225),
+points int,
+idLivre int,
+index(idQuestion))engine=innodb;
+
+/* suppression puis création de la table propositon */
+drop table if exists proposition;
+
+create table proposition(
+idProposition int not null auto_increment,
+idQuestion int,
+proposition varchar(255),
+solution int,
+index(idProposition))engine=innodb;
+
+/* suppression puis création de la table rallye */
+drop table if exists rallye;
+
+create table rallye(
+idRallye int,
+dateDebut date,
+duree int,
+theme varchar (45))engine=innodb;
+
+/* suppression puis création de la table comporter */
+drop table if exists comporter;
+
+create table comporter(
+idLivre int,
+idRallye int)engine=innodb;
+
+/* suppression puis création de la table classe */
+drop table if exists classe;
+
+create table classe(
+idClasse int not null auto_increment,
+idEnseignant int,
+anneeScolaire varchar (45),
+idNiveau int,
+index (idClasse))engine=innodb;
+
+/* suppression puis création de la table enseignant */
+drop table if exists niveau;
+
+create table niveau(
+idNiveau int not null auto_increment,
+niveauScolaire varchar(45),
+index(idNiveau))engine=innodb;
+
+/* suppression puis création de la table eleve */
+drop table if exists eleve;
+
+create table eleve(
+    idEleve int not null auto_increment,
+    idClasse int,
+    nom varchar (30),
+    prenom varchar (20),
+    loginv varchar (100),
+    idAuth int,
+    index (idEleve))engine=innodb;
+
+/* suppression puis création de la table participer */
+drop table if exists participer;
+
+create table participer(
+idRallye int,
+idEleve int)engine=innodb;
+
+/* suppression puis création de la table reponse */
+drop table if exists reponse;
+
+create table reponse(
+idParticiperRallye int,
+idParticiperEleve int,
+idQuestion int,
+idProposition int)engine=innodb;
+
+drop table if exists enseignant;
+
+create table enseignant(
+idEnseignant int not null auto_increment,
+nom varchar (30),
+prenom varchar (45),
+loginE varchar (100),
+idAuth int,
+index (idEnseignant))engine=innodb;
 
 
 
--- bddRallyeLecture.sql
+++ bddRallyeLecture.sql
@@ -1,158 +1,146 @@
-/* création de la database */
-drop DATABASE if exists RallyeLecture;
-CREATE DATABASE RallyeLecture;
+drop database if exists rallyeLecture;
+create database rallyeLecture;
+use rallyeLecture;
 
-/* on se positionne sur la base Rallye */
-use RallyeLecture;
+/* Mettre les tables d'authentifications avant! */
 
-
-/* suppression puis création de la table livre */
-drop table if exists livre;
-
-create table livre(
-idLivre int not null auto_increment,
-titre varchar (45),
-couverture varchar (100),
-idAuteur int,
-idEditeur int,
-index (idLivre))engine=innodb;
-
-/* old code
-primary key(idLivre),
-foreign key (idEditeur) references editeur(idEditeur),
-foreign key (idAuteur) references auteur(idAuteur))engine=innodb;
-*/
-
-/* suppression puis création de la table éditeur */
 drop table if exists editeur;
-
 create table editeur(
-idEditeur int auto_increment not null,
-nom varchar (60),
-index (idEditeur))engine=innodb;
+    id int not null auto_increment,
+    nom varchar(60),
+    primary key(id)
+)engine=innodb;
 
-/* suppression puis création de la table auteur */
 drop table if exists auteur;
-
 create table auteur(
-idAuteur int auto_increment not null,
-nom varchar (60),
-index (idAuteur))engine=innodb;
+    id int not null auto_increment,
+    nom varchar(60),
+    primary key(id)
+)engine=innodb;
 
-/* suppression puis création de la table question */
+drop table if exists livre;
+create table livre(
+    id int not null auto_increment,
+    titre varchar(45),
+    couverture varchar(100),
+    idAuteur int,
+    idEditeur int,
+    primary key(id)
+)engine=innodb;
+
 drop table if exists question;
-
 create table question(
-idQuestion int not null auto_increment,
-question varchar (225),
-points int,
-idLivre int,
-index(idQuestion))engine=innodb;
-/*
-foreign key (idLivre) references livre (idLivre)*/
+    id int not null auto_increment,
+    question varchar(255),
+    points int,
+    idLivre int,
+    primary key(id)
+)engine=innodb;
 
-/* suppression puis création de la table propositon */
-drop table if exists proposition;
-
-create table proposition(
-idProposition int not null auto_increment,
-idQuestion int,
-proposition varchar(255),
-solution int,
-index(idProposition))engine=innodb;
-/* foreign key (idQuestion) references question (idQuestion)*/
-
-
-
-/* suppression puis création de la table rallye */
 drop table if exists rallye;
-
 create table rallye(
-idRallye int,
-dateDebut date,
-duree int,
-theme varchar (45))engine=innodb;
+    id int not null auto_increment,
+    dateDebut Date,
+    duree int,
+    theme varchar(45),
+    primary key(id)
+)engine=innodb;
 
-/* suppression puis création de la table comporter */
 drop table if exists comporter;
-
 create table comporter(
-idLivre int,
-idRallye int)engine=innodb;
-/*foreign key (idLivre) references Livre (idLivre),
-foreign key (idRallye) references Rallye (idRallye)*/
+    idLivre int not null,
+    idRallye int not null,
+    primary key(idLivre, idRallye)
+)engine=innodb;
 
-/* suppression puis création de la table classe */
-drop table if exists classe;
-
-create table classe(
-idClasse int,
-idEnseignant int,
-anneeScolaire varchar (45),
-idNiveau int)engine=innodb;
-
-/*primary key(idClasse),
-foreign key (idEnseignant) references enseignant(idEnseignant),
-foreign key (idNiveau) references niveau(idEnseignant)*/
-
-
-/* suppression puis création de la table enseignant */
-drop table if exists niveau;
-
-create table niveau(
-idNiveau int,
-niveauScolaire varchar(45))engine=innodb;
-
-/*primary key(idNiveau)*/
-
-/* suppression puis création de la table eleve */
-drop table if exists eleve;
-
-create table eleve(
-    idEleve int not null auto_increment,
-    idClasse int,
-    nom varchar (30),
-    prenom varchar (20),
-    loginv varchar (100),
-    idAuth int,
-    index (idEleve))engine=innodb;
-
-    /*primary key (idEleve),
-    foreign key (idClasse) references classe (idClasse)*/
-
-
-
-/* suppression puis création de la table participer */
-drop table if exists participer;
-
-create table participer(
-idRallye int,
-idEleve int)engine=innodb;
-
-/*foreign key (idRallye) references Rallye (idRallye),
-foreign key (idEleve) references Eleve (idEleve)*/
-
-/* suppression puis création de la table reponse */
-drop table if exists reponse;
-
-create table reponse(
-idParticiperRallye int,
-idParticiperEleve int,
-idQuestion int,
-idProposition int)engine=innodb;
-/*primary key (idParticiperEleve),
-primary key (idParticiperRallye),
-foreign key (idProposition) references proposition (idProposition)*/
-/* suppression puis création de la table enseignant */
+drop table if exists proposition;
+create table proposition(
+    id int not null auto_increment,
+    idQuestion int,
+    proposition varchar(255),
+    solution int,
+    primary key(id)
+)engine=innodb;
 
 drop table if exists enseignant;
-
 create table enseignant(
-idEnseignant int,
-nom varchar (30),
-prenom varchar (45),
-loginE varchar (100),
-idAuth int)engine=innodb;
-/*primary key(idEnseignant)*/
+    id int auto_increment not null,
+    nom varchar(45),
+    prenom varchar(45),
+    login varchar(100),
+    idAuth int,
+    primary key (id))engine=innodb;
+
+drop table if exists niveau;
+create table niveau(
+    id int auto_increment not null,
+    niveauScolaire varchar(45),
+    primary key (id))engine=innodb;
+    
+
+drop table if exists classe;
+create table classe(
+    id int auto_increment not null,
+    idEnseignant int,
+    anneeScolaire varchar(45),
+    idNiveau int,
+    primary key (id))engine=innodb;
+
+drop table if exists eleve;
+create table eleve(
+    id int auto_increment not null,
+    idClasse int,
+    nom varchar(45),
+    prenom varchar(45),
+    login varchar(45),
+    idAuth int,
+    primary key (id))engine=innodb;
+
+drop table if exists participer;
+create table participer(
+     idRallye int not null,
+     idEleve int not null,
+     primary key (idRallye, idEleve))engine=innodb;
+
+
+drop table if exists reponse;
+create table reponse(
+     idParticiperRallye int not null,
+     idParticiperEleve int not null,
+     idQuestion int not null,
+     idProposition int not null,
+     primary key (idParticiperRallye, idParticiperEleve, idQuestion, idProposition))engine=innodb;
+
+alter table participer
+add constraint fk_participer_idEleve
+foreign key(idEleve) references eleve(id);
+
+alter table participer
+add constraint fk_participer_idRallye
+foreign key(idRallye) references rallye(id);
+
+alter table livre add constraint fk_livre_idAuteur foreign key(idAuteur) references auteur(id);
+alter table livre add constraint fk_livre_idEditeur foreign key(idEditeur) references editeur(id);
+
+alter table question add constraint fk_question_idLivre foreign key(idLivre) references livre(id);
+
+alter table comporter add constraint fk_comporter_idLivre foreign key(idLivre) references livre(id);
+alter table comporter add constraint fk_comporter_idRallye foreign key(idRallye) references rallye(id);
+
+alter table classe add constraint fk_classe_idEnseignant foreign key(idEnseignant) references enseignant(id);
+alter table classe add constraint fk_classe_idNiveau foreign key(idNiveau) references niveau(id);
+
+alter table eleve add constraint fk_eleve_idClasse foreign key(idClasse) references classe(id);
+alter table eleve add constraint fk_eleve_idAuth foreign key(idAuth) references aauth_users(id);
+
+alter table proposition add constraint fk_proposition_idQuestion foreign key(idQuestion) references question(id);
+
+alter table reponse add constraint fk_reponse_idParticiperRallye foreign key(idParticiperRallye) references participer(idRallye);
+alter table reponse add constraint fk_reponse_idParticiperEleve foreign key(idParticiperEleve) references participer(idEleve);
+alter table reponse add constraint fk_reponse_idQuestion foreign key(idQuestion) references proposition(idQuestion);
+alter table reponse add constraint fk_reponse_idProposition foreign key(idProposition) references proposition(id);
+
+
+
 
 
